70228775a3168366536f7d0ca67b17f1c4bd42f8
Nick <35607793+nicholasnikolov90@users.noreply.github.com>
2023-06-21 18:43:59 -0600

    wires calculating correctly

--- abaqusPoints.py
+++ abaqusPoints.py
@@ -8,16 +8,21 @@ coordinates.columns.values[1] = 'y'
 coordinates.columns.values[2] = 'z'
 coordinates = coordinates.fillna(0).div(1000).sort_index()
 
-points = pd.Series() #initialize series object
-#Initialize first two unique rows
-points.at[0] = f"mdb.models['Model-1'].parts['Part-1'].ReferencePoint(point=({coordinates.loc[0, 'x']},{coordinates.loc[0, 'y']},{coordinates.loc[0, 'z']}))"
-points.at[1] = f"mdb.models['Model-1'].parts['Part-1'].DatumPointByOffset(point=mdb.models['Model-1'].parts['Part-1'].referencePoints[1], vector=({coordinates.loc[1, 'x']},{coordinates.loc[1, 'y']},{coordinates.loc[1, 'z']}))"
+points_wires = pd.Series() #initialize series object
+#Initialize first two unique rows of points
+points_wires.at[0] = f"mdb.models['Model-1'].parts['Part-1'].ReferencePoint(point=({coordinates.loc[0, 'x']},{coordinates.loc[0, 'y']},{coordinates.loc[0, 'z']}))"
+points_wires.at[1] = f"mdb.models['Model-1'].parts['Part-1'].DatumPointByOffset(point=mdb.models['Model-1'].parts['Part-1'].referencePoints[1], vector=({coordinates.loc[1, 'x']},{coordinates.loc[1, 'y']},{coordinates.loc[1, 'z']}))"
 
 #loop through entire coordinates, create all remaining points
 for i in range(2, coordinates.index.max()+1):
-    print(i)
-    points.at[i] = f"mdb.models['Model-1'].parts['Part-1'].DatumPointByOffset(point=mdb.models['Model-1'].parts['Part-1'].datums[{i}], vector=({coordinates.loc[i, 'x']},{coordinates.loc[i, 'y']},{coordinates.loc[i, 'z']}))"
+    points_wires.at[i] = f"mdb.models['Model-1'].parts['Part-1'].DatumPointByOffset(point=mdb.models['Model-1'].parts['Part-1'].datums[{i}], vector=({coordinates.loc[i, 'x']},{coordinates.loc[i, 'y']},{coordinates.loc[i, 'z']}))"
+
+#Initialize first unique row of wires
+points_wires.at[coordinates.index.max()+2] = f"mdb.models['Model-1'].parts['Part-1'].WirePolyLine(mergeType=IMPRINT, meshable=ON, points=((mdb.models['Model-1'].parts['Part-1'].referencePoints[1],mdb.models['Model-1'].parts['Part-1'].datums[2]), ))"
+
+for i in range(coordinates.index.max()+3, coordinates.index.max()*2+2):
+    points_wires.at[i] = f"mdb.models['Model-1'].parts['Part-1'].WirePolyLine(mergeType=IMPRINT, meshable=ON, points=((mdb.models['Model-1'].parts['Part-1'].datums[{i-1-coordinates.index.max()}],mdb.models['Model-1'].parts['Part-1'].datums[{i-coordinates.index.max()}]), ))"
 
 #save points to a .txt file
-np.savetxt(r'np.txt', points, fmt = '%s')
+np.savetxt(r'np.txt', points_wires, fmt = '%s')
 
